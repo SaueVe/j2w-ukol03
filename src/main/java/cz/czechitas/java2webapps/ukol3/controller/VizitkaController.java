27fa5926bc9e29581f61815e785f7caafa6b8f31
veronikasauerova <verca.105@seznam.cz>
2025-10-10 12:25:33 +0200

    3. ukol hotovo

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -15,9 +15,9 @@ import java.util.List;
 public class VizitkaController {
     private final List<Vizitka> seznamVizitek=List.of(
             new Vizitka("Dita (Přikrylová) Formánková", "Czechitas z. s.", "Podlipného 810/14", "Praha 8-Libeň, 180 00","dita@czechitas.cs","+420 800 123 456", "www.czechitas.cz"),
-            new Vizitka("Barbora Bühnová", "Czechitas z. s.", "Sokolovská 139/320", "Praha 9, 190 00","","+420 294 367 948", "www.czechitas.cz"),
+            new Vizitka("Barbora Bühnová", "Czechitas z. s.", "Sokolovská 139/320", "Praha 9, 190 00",null,"+420 294 367 948", "www.czechitas.cz"),
             new Vizitka("Monika Ptáčníková", "Czechitas z. s.", "Botanická 828/45", "Brno-střed-Veveří, 602 00","monika@czechitas.cs","+420 763 947", "www.czechitas.cz"),
-            new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Klímova 2127/4", "Brno-Žabovřesky, 616 00","mirka@czechitas.cs","", "www.czechitas.cz")
+            new Vizitka("Mirka Zatloukalová", "Czechitas z. s.", "Klímova 2127/4", "Brno-Žabovřesky, 616 00","mirka@czechitas.cs",null, "www.czechitas.cz")
     );
 
     @GetMapping("/")
